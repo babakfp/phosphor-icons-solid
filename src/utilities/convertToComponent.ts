c70389c0be70f96914449a1fba8a84f10361b402
babakfp <babak.bxf@gmail.com>
2025-05-06 20:20:12 +0330

    fix: `className` to `class`

--- src/utilities/convertToComponent.ts
+++ src/utilities/convertToComponent.ts
@@ -1,9 +1,8 @@
 export const convertToComponent = (svg: string, iconName: string) => {
-    let newSvg = `export default ({ className }: { className?: string }) => (`
+    let newSvg = `export default (p: { class?: string }) => (`
     const attrs =
-        `class={className} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" pointer-events="none" display="inline-block"`
+        `class={p.class} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" pointer-events="none" display="inline-block"`
     newSvg += svg.replace("<svg", `<svg ${attrs}`)
     newSvg += ")"
-
     return newSvg
 }
